4e9431d0202737247d8e6d96905a24ecaf05e431
tmcken <tmcken@sandia.gov>
2018-12-12 13:09:45 -0700

    NA

--- cran_downloads/cran_downloads_pull.R
+++ cran_downloads/cran_downloads_pull.R
@@ -5,7 +5,7 @@ setwd("~/git/snfa-scripts/cran_downloads")
 rm(list = ls())
 
 start = as.Date('2018-12-01')
-today = as.Date('2018-12-09')
+today = as.Date('2018-12-11')
 
 all.days = as.character(seq(start, today, by = 'day'))
 existing.days = gsub(".csv.gz$", "", grep(".csv.gz$", list.files("logs"), value = TRUE))
